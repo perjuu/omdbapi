18d8bdfb57f93f4581a38a64d56702f8d1f7485e
perjuu <timotei.perju@verticaldigital.ca>
2022-06-10 11:10:48 +0300

    All done

---
+++ script.js
@@ -0,0 +1,31 @@
+var data;
+function getname(q) {
+  $.get(
+    "http://www.omdbapi.com/?t=" + q + "&apikey=b958bc89",
+    function (rawdata) {
+      var rawstring = JSON.stringify(rawdata);
+      data = JSON.parse(rawstring);
+
+      var title = data.Title;
+      var year = data.Year;
+      var imdburl = "https://www.imdb.com/title/" + data.imdbID + "/";
+      var posterurl = data.Poster;
+      var genre = data.Genre;
+
+      document.getElementById("answer").innerHTML =
+        "<hr><h1> Title: " +
+        title +
+        "</h1> <img src= '" +
+        posterurl +
+        "'><br><p> Tip: <strong>" +
+        genre +
+        "</strong><br> Year: <strong>" +
+        year +
+        "</strong></p><p> IMDB page: <a href='" +
+        imdburl +
+        "'target='_blank'>" +
+        imdburl +
+        "</a></p>";
+    }
+  );
+}
